80fd62d7c06e5d60a39168c9ac9e4db21d3f1a31
kdrbipul <kdrbipul@gmail.com>
2023-06-18 13:44:19 +0600

    all done

--- js/index.js
+++ js/index.js
@@ -4,3 +4,13 @@ if(temperature>=30){
 }else{
     console.log("It's not a hot day");
 }
+
+
+let time = 14;
+if(time<12){
+    console.log("Good Morning");
+}else if (time<18){
+    console.log("Good Afternoon");
+}else{
+    console.log("Good Evening");
+}
